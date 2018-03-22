9e5aa97b2000c4f0e24cced1b2b5d426cf031838
Clément Pinard <clement.pinard@parrot.com>
2018-03-22 11:34:46 +0100

    add pynvml3 dependency

--- setup.py
+++ setup.py
@@ -1,11 +1,12 @@
-from distutils.core import setup
-from sys import version
+from setuptools import setup
 
 setup(name='super-smi',
       description='Python Bindings for the NVIDIA Management Library',
-      py_modules=['pynvml', 'nvidia_smi', 'my_smi'],
+      py_modules=['my_smi'],
       scripts=['server_smi.py','client_smi.py','client_smi_appindicator.py'],
       data_files=[('data',['empty.png','hosts_to_smi.json']),
-                  ('/etc/systemd/system', ['server_smi.service'])]
+                  ('/etc/systemd/system', ['server_smi.service'])],
+      install_requires=[
+          'nvidia-ml-py3',
+      ]
       )
-
