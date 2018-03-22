5a0b6d8efaeb4adbc815d7a2593e4b084fbbef92
Clément Pinard <clement.pinard@parrot.com>
2018-03-22 12:00:24 +0100

    better package metadata

--- setup.py
+++ setup.py
@@ -1,7 +1,12 @@
 from setuptools import setup
 
-setup(name='super-smi',
-      description='Python Bindings for the NVIDIA Management Library',
+setup(name='nvidia-multiple-smi',
+      version='1.1',
+      url='https://github.com/ClementPinard/nvidia-multiple-smi',
+      license='MIT',
+      author='Clément Pinard',
+      author_email='clempinard@gmail.com',
+      description='Python Bindings for the NVIDIA Management Library over the network',
       py_modules=['my_smi'],
       scripts=['server_smi.py','client_smi.py','client_smi_appindicator.py'],
       data_files=[('data',['empty.png','hosts_to_smi.json']),
